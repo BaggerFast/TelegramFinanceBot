2233158adcc27cd910cc061adfe33a314b5719ae
baggerfast <riosha3@gmail.com>
2022-07-29 16:00:10 +0300

    ADD: Env token in config.py, start bot in run.py

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
+from aiogram import Bot, Dispatcher, executor
+from loguru import logger
+
+from bot import TgBot
+
+
 def main():
-    pass
+    bot = Bot(token=TgBot.TOKEN, parse_mode='HTML')
+    dp = Dispatcher(bot)
+    logger.info('Bot start')
+    executor.start_polling(dp, skip_updates=True)
 
 
 if __name__ == "__main__":
